5e348cb33d5e25d82ef491019b179d387c2dae80
SunCY <1428184913@qq.com>
2021-01-21 23:49:22 +0800

    还差下拉列表没写好

--- admin-dev/src/components/myHomePage.vue
+++ admin-dev/src/components/myHomePage.vue
@@ -2,25 +2,26 @@
   <div id = "homepage_containter">
     <div class = 'nav'> 
       <div class = 'nav_pic'><img src = "../assets/mss.png"></div>
-      <div class = 'nav_others'>
-        
-        <a href=""><img src = "../assets/sy.png"></a> 
-        <a href=""><img src = "../assets/sqjx.png"></a> 
-        <a href=""><img src = "../assets/ywhz.png"></a>
-        <a href=""><img src = "../assets/fbzgj.png"></a>
-        <a href=""><img src = "../assets/xwzx.png"></a>
-        <a href=""><img src = "../assets/gywm.png"></a> 
-        <a href=""><img src = "../assets/shzr.png"></a> 
-        <a href=""><img src = "../assets/jrwm.png"></a> 
-        
+      <div class = 'nav_others'> 
+        <div class = "add_bottom_line"><a href=""><img src = "../assets/sy.png"></a> </div>
+        <div class = "add_bottom_line"><a href=""><img src = "../assets/sqjx.png"></a> </div>
+        <div class = "add_bottom_line"><a href=""><img src = "../assets/ywhz.png"></a> </div>
+        <div class = "add_bottom_line"><a href=""><img src = "../assets/fbzgj.png"></a> </div>
+        <div class = "add_bottom_line"><a href=""><img src = "../assets/xwzx.png"></a> </div>
+        <div class = "add_bottom_line"><a href=""><img src = "../assets/gywm.png"></a> </div>
+        <div class = "add_bottom_line"><a href=""><img src = "../assets/shzr.png"></a> </div>
+        <div class = "add_bottom_line"><a href=""><img src = "../assets/jrwm.png"></a> </div> 
       </div>
     </div>
     <div class = 'content'>
       <div class = 'content_left'><img src = "../assets/phone.png"></div>
-      <div class = 'content_right'>
-        <div class = 'content_right_top'><img src = "../assets/logo.png"></div>
-        <div class = 'content_right_middle'> <div class = 'add_line'><img src = "../assets/hfyp+yhdsj.png"></div></div>
-        <div class = 'content_right_bottom'><img src = "../assets/ios.png"><img src = "../assets/android.png"></div> 
+      <div class = 'content_right'> 
+          <div class = 'content_right_top'><img src = "../assets/logo.png"></div>
+          <div class = 'content_right_middle'> <div class = 'add_line'><img src = "../assets/hfyp+yhdsj.png"></div></div>
+          <div class = 'content_right_bottom'>
+            <a href = ""><img src = "../assets/ios.png"></a>
+            <a href = ""><img src = "../assets/android.png"></a>
+          </div>   
       </div>
     </div>
   </div>
@@ -43,82 +44,94 @@ export default {
   height:100%;
 } 
 .nav{ 
-  background-color:rgba(69, 84, 131,0.2);
+  /* background-color:rgba(69, 84, 131,0.2); */
   height:80px;
   width:100%;
   float: left;
 }
-.nav_pic{
-  float: left;
-  margin-left: 30px;
-  margin-top:20px;
-  width:30%;
+.add_bottom_line{
+  text-align: center;
+  float: left;  
+  margin-left: 20px;
+  margin-right: 20px;
+  height:30px;
 }
-.nav_others{
+.nav_pic{
+  float: left; 
+  margin-top: 20px;
+}
+.nav_others{ 
+  float: right;
   padding-top: 20px;
 }
-.nav_others a{
-  margin-left: 20px; 
+
+.add_bottom_line:hover{
+  border-bottom: 2px solid rgb(255, 255, 255); 
 }
-.content{   
-  height:80%;
-  width:100%;
-  display: flex;
-  align-items: center;
-  justify-content: center;
+.nav_others a{ 
 }
-.content_left{ 
-  text-align: center;
+.content{    
+  height:80%; 
+}
+.content_left{  
   float: left;
-  background-color:rgba(65, 225, 190,0.3);
+  text-align: right;
+  /* background-color:rgba(65, 225, 190,0.3); */
+  margin-left: 15%;
   height:100%;
-  width:40%;
+  width:30%;
 }
 .content_left img{
   height:90%;
   margin-top:20px;
+   margin-right: 15%;
 }
 .content_right{
   float: left; 
   height:100%;
-  width:40%;
-}
+  width:32%;
+  /* background-color: rgba(100, 84, 82, 0.3); */
+  margin-left: 2%;
+} 
 .content_right_top{ 
-  height:60%;
+ 
+  height:50%;
   width:100%;
   text-align: center;
-  background-color: rgba(65, 50, 49,0.3);
+  /* background-color: rgba(65, 50, 49,0.3); */
 }
 .content_right_top img{
-  height:100%;
+  margin-top:50px;
+  height:84%;
 }
 .content_right_middle{ 
-  height:20%;
+  height:15%;
   width:100%;
-  background-color: rgba(114, 245, 250,0.2);
+  /* background-color: rgba(114, 245, 250,0.2); */
   text-align: center;
-  display: flex;
-  align-items: center;
-  justify-content: center;
-}
-.content_right_middle img{ 
-  width:100%;
-  margin-top:20px;
-}
-.add_line{
-  width:80%;
-  height: 100%;
-  border-top :4px solid rgb(255, 255, 255) ;
-  margin-top:15px; 
-}
-.content_right_bottom{ 
-  height:20%;
-  width:100%;
-  background-color: rgba(250, 114, 232,0.2);
-  
+  margin-top: 5%;
   display: flex;
   align-items: center;
   justify-content: center;
 }
 
+.content_right_middle img{ 
+  width:100%;
+  margin-top:20px;
+}
+.add_line{
+  width:100%;
+  height: 100%;
+  /* border-top :3px solid rgb(255, 255, 255) ;  */
+}
+.content_right_bottom{ 
+  height:25%;
+  width:100%;
+  /* background-color: rgba(250, 114, 232,0.2);  */
+  display: flex;
+  justify-content: space-evenly;
+}
+.content_right_bottom img{ 
+  height:40px; 
+}
 </style>
