7746e7016bb17bb7df62204efcf52e5a110b5277
SunCY <1428184913@qq.com>
2021-01-22 10:38:48 +0800

    在服务器上菜单栏异常

--- admin-dev/src/components/myHomePage.vue
+++ admin-dev/src/components/myHomePage.vue
@@ -4,9 +4,27 @@
       <div class = 'nav_pic'><img src = "../assets/mss.png"></div>
       <div class = 'nav_others'> 
         <div class = "add_bottom_line"><a href=""><img src = "../assets/sy.png"></a> </div>
-        <div class = "add_bottom_line"><a href=""><img src = "../assets/sqjx.png"></a> </div>
-        <div class = "add_bottom_line"><a href=""><img src = "../assets/ywhz.png"></a> </div>
-        <div class = "add_bottom_line"><a href=""><img src = "../assets/fbzgj.png"></a> </div>
+        <div class = "add_bottom_line"><a href=""><img src = "../assets/sqjx.png"></a> </div> 
+        <div class = "add_bottom_line_special"> 
+          <el-menu :default-active="activeIndex" class="el-menu-demo" mode="horizontal" @select="handleSelect"> 
+          <el-submenu index="2">
+              <template slot="title"  background-color:red ><img src = "../assets/ywhz.png"></template>
+              <el-menu-item index="2-1">合作1</el-menu-item>
+              <el-menu-item index="2-2">合作2</el-menu-item>
+              <el-menu-item index="2-3">合作3</el-menu-item> 
+          </el-submenu> 
+          </el-menu> 
+        </div>
+        <div class = "add_bottom_line_special"> 
+          <el-menu :default-active="activeIndex" class="el-menu-demo" mode="horizontal" @select="handleSelect"> 
+          <el-submenu index="2">
+              <template slot="title"  background-color:red ><img src = "../assets/fbzgj.png"></template>
+              <el-menu-item index="2-1">工具1</el-menu-item>
+              <el-menu-item index="2-2">工具2</el-menu-item>
+              <el-menu-item index="2-3">工具3</el-menu-item> 
+          </el-submenu> 
+          </el-menu> 
+        </div>
         <div class = "add_bottom_line"><a href=""><img src = "../assets/xwzx.png"></a> </div>
         <div class = "add_bottom_line"><a href=""><img src = "../assets/gywm.png"></a> </div>
         <div class = "add_bottom_line"><a href=""><img src = "../assets/shzr.png"></a> </div>
@@ -28,16 +46,60 @@
 </template>
 
 <script>
-export default { 
-  data () {
-    return {
-      msg: 'Welcome to HomePage'
+  export default {
+    data() {
+      return {
+        activeIndex: '1',
+        activeIndex2: '1'
+      };
+    },
+    methods: {
+      handleSelect(key, keyPath) {
+        console.log(key, keyPath);
+      }
     }
   }
-}
 </script>
 
-<style scoped>
+<style> 
+ .wrapper{
+    background-color: violet;
+    height: 200px;
+    float:left;
+}
+.el-menu-demo{
+    height: 40px; 
+    background:transparent;
+    /* border-bottom-color:none!important; */
+    background-color:rgba(0,0,0,0);
+    float:right; 
+
+}
+/* 点击出来的下划线进行隐藏 */
+.el-menu-item.is-active {
+    border-bottom: none!important;
+    color: rgba(0,0,0,0);
+}
+/* 整体的下划线进行隐藏 */
+.el-menu.el-menu--horizontal{
+  border-bottom: none;
+}
+/* 导航栏的间距等样式 */
+.el-menu-item {
+  padding: 0 62px;
+  font-size: 16px;
+}
+
+/* 点击出来的下划线动效残留进行隐藏 */
+.el-menu--horizontal>.el-menu-item{
+    border-bottom: none;
+}
+/* 点击以后的背景色进行隐藏 */
+.el-menu--horizontal>.el-menu-item:not(.is-disabled):focus, .el-menu--horizontal>.el-menu-item:not(.is-disabled):hover, .el-menu--horizontal>.el-submenu .el-submenu__title:hover {
+    background-color: rgba(0,0,0,0); 
+}
+
+
 #homepage_containter{
   background-image: url("../assets/background_pic.png");
   background-size: 100% 100%;
@@ -56,6 +118,11 @@ export default {
   margin-right: 20px;
   height:30px;
 }
+.add_bottom_line_special{
+  float: left;  
+  margin-top: -19px;
+  margin-right: -12px;
+}
 .nav_pic{
   float: left; 
   margin-top: 20px;
@@ -67,8 +134,6 @@ export default {
 
 .add_bottom_line:hover{
   border-bottom: 2px solid rgb(255, 255, 255); 
-}
-.nav_others a{ 
 }
 .content{    
   height:80%; 
@@ -122,7 +187,7 @@ export default {
 .add_line{
   width:100%;
   height: 100%;
-  /* border-top :3px solid rgb(255, 255, 255) ;  */
+  border-top :3px solid rgb(255, 255, 255) ; 
 }
 .content_right_bottom{ 
   height:25%;
@@ -134,4 +199,5 @@ export default {
 .content_right_bottom img{ 
   height:40px; 
 }
+  
 </style>
---
+++ admin-dev/src/components/testPage.vue
@@ -0,0 +1,68 @@
+<template>
+    <div class = "wrapper">
+        <el-menu :default-active="activeIndex" class="el-menu-demo" mode="horizontal" @select="handleSelect"> 
+        <el-submenu index="2">
+            <template slot="title"  background-color:red ><img src = "../assets/fbzgj.png"></template>
+            <el-menu-item index="2-1"><a href ="">选项1</a></el-menu-item>
+            <el-menu-item index="2-2">选项2</el-menu-item>
+            <el-menu-item index="2-3">选项3</el-menu-item> 
+        </el-submenu> 
+        </el-menu> 
+    </div>
+
+</template>
+
+<script> 
+  export default {
+    data() {
+      return {
+        activeIndex: '1',
+        activeIndex2: '1'
+      };
+    },
+    methods: {
+      handleSelect(key, keyPath) {
+        console.log(key, keyPath);
+      }
+    }
+  }
+</script>
+
+<style>
+.wrapper{
+    background-color: violet;
+    height: 200px;
+    float:left;
+}
+.el-menu-demo{
+    height: 40px;
+    background:transparent;
+    /* border-bottom-color:none!important; */
+    background-color:rgba(0,0,0,0);
+    float:right; 
+
+}
+/* 点击出来的下划线进行隐藏 */
+.el-menu-item.is-active {
+    border-bottom: none!important;
+    color: rgba(0,0,0,0);
+}
+/* 整体的下划线进行隐藏 */
+.el-menu.el-menu--horizontal{
+  border-bottom: none;
+}
+/* 导航栏的间距等样式 */
+.el-menu-item {
+  padding: 0 62px;
+  font-size: 16px;
+}
+
+/* 点击出来的下划线动效残留进行隐藏 */
+.el-menu--horizontal>.el-menu-item{
+    border-bottom: none;
+}
+/* 点击以后的背景色进行隐藏 */
+.el-menu--horizontal>.el-menu-item:not(.is-disabled):focus, .el-menu--horizontal>.el-menu-item:not(.is-disabled):hover, .el-menu--horizontal>.el-submenu .el-submenu__title:hover {
+    background-color: rgba(0,0,0,0); 
+}
+</style>
